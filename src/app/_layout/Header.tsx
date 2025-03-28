e9f1f915ea810c852e90e1639dc79c57d3edfee1
Kens04 <kento090403@icloud.com>
2025-03-28 22:38:12 +0900

    ヘッダーレイアウト調整

--- src/app/_layout/Header.tsx
+++ src/app/_layout/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Header: React.FC = () => {
   return (
-    <div className="bg-slate-900 p-4">
+    <div className="bg-slate-900 p-4 fixed z-[1] w-full top-0 left-0">
       <div className="flex justify-between">
         <div>
           <Link className="text-white" href="/">
