15e599e3af6a4ce63608a1cd48750f119745f640
Kens04 <kento090403@icloud.com>
2025-04-01 22:31:22 +0900

    サイドバーレイアウト調整

--- src/app/_layout/Sidebar.tsx
+++ src/app/_layout/Sidebar.tsx
@@ -5,7 +5,7 @@ export const Sidebar: React.FC = () => {
     <div className="bg-slate-100 h-screen w-48 flex flex-col fixed top-0 left-0 pt-[56px]">
       <div className="w-full">
         <Link
-          className="block w-full text-center font-bold py-2 px-4 hover:bg-slate-600 hover:text-white"
+          className="block w-full font-bold py-2 px-4 hover:bg-slate-600 hover:text-white"
           href="/admin/posts"
         >
           記事一覧
@@ -13,7 +13,7 @@ export const Sidebar: React.FC = () => {
       </div>
       <div className="w-full">
         <Link
-          className="block w-full text-center font-bold py-2 px-4 hover:bg-slate-600 hover:text-white"
+          className="block w-full font-bold py-2 px-4 hover:bg-slate-600 hover:text-white"
           href="/admin/categories"
         >
           カテゴリー一覧
